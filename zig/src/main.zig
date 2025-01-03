5511d8ce3574b1bfb94edad9ba8e33e7b3be61cd
Christopher Seven Phiri <chris7phiri@gmail.com>
2025-01-03 22:19:32 +0200

    init

---
+++ zig/src/main.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var file = try std.fs.cwd().openFile("content/test.json", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    const writer = line.writer();
+    var line_no: usize = 0;
+
+    while (reader.streamUntilDelimiter(writer, '\n', null)) {
+        defer line.clearRetainingCapacity();
+        line_no += 1;
+
+        print("{d}--{s}\n", .{ line_no, line.items });
+    } else |err| switch (err) {
+        error.EndOfStream => { // end of file
+            if (line.items.len > 0) {
+                line_no += 1;
+                print("{d}--{s}\n", .{ line_no, line.items });
+            }
+        },
+        else => return err, // Propagate error
+    }
+
+    print("Total lines: {d}\n", .{line_no});
+}
